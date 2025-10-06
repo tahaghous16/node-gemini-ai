cee7733fc44676ff8930c6e17361bba2cc23c2f7
Taha Ghous <tahaghous2002@gmail.com>
2025-10-06 19:57:03 +0500

    Remove all files from GitHub

--- routes/ai.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { aiController } from "../controller/ai.controller.js";
-
-const aiRouter = express.Router();
-
-aiRouter.post("/generate-content", aiController);
-
-export default aiRouter;
